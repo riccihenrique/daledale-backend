5b879c5a127ecd2628bbf9a49f4f1346e157bd67
Henrique <ricci.henrique@outlook.com>
2021-05-27 18:26:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Começou o fim de semana Command Line Sound
+
+
+## Compatibility
+
+- Linux
+- Windows
+- Mac
+
+## Installing
+Install globally:
+
+    npm install -g daledale-backend
+
+## Running
+Run command:
+
+    daledalebackend
+
+Or import in NodeJS script:
+
+    import daledalebackend from 'daledalebackend';
+
+    daledalebackend();
+    
+## Running with [npx](https://www.npmjs.com/package/npx)
+Run command:
+
+    npx daledale-backend
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const exect = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+
+const R = require('ramda');
+
+const mainPath = path.dirname(fs.realpathSync(__filename));
+const soundPath = path.join(mainPath, './daledale-backend');
+
+const daledalebackend = function (){
+    const linuxcmd = R.join('', ['paplay ', soundPath, '.ogg']);
+    const windowscmd = R.join('', [path.join(mainPath, './forWindows.vbs'), ' ', soundPath, '.mp3']);
+    const maccmd = R.join('', ['afplay ', soundPath, '.mp3']);
+
+    const platform = process.platform;
+
+    R.cond([
+        [R.equals('linux'), exec(linuxcmd)],
+        [R.equals('win32'), exec(windowscmd)],
+        [R.equals('darwin'), exec(maccmd)],
+    ], platform)
+
+    function exec(cmd){
+        return exect(cmd, function (error) {
+            R.ifElse(
+               R.empty,
+               () => console.log('COMEÇOU O FIM DE SEMANA!! #PAS'),
+               (error) => console.error(error),
+               error)
+        });
+    }
+}
+
+module.exports = daledalebackend;
+
+if (!module.parent) {
+    daledalebackend();
+}
